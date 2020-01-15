971cda3d36e3a34119e816ed5bcbab05aa1249d7
mcespiva <michal.cespiva@atlas.cz>
2020-01-15 17:26:06 +0100

    after lecture 97

---
+++ tests/setup.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const keys = require('../config/keys');
+require('../models/User');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+
